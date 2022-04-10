60b0243123bc3168c7345cb8616208355b123dad
Kevin Wither <kvn.wither@gmail.com>
2022-04-10 15:35:33 -0600

    commit tree 98737fbffb1cfa40cc46520392e47143236e9319

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, string) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = (`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+    }
+    return cards;
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i)
+        i--
+    };
+}
+
+
